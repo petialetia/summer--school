ac3d30e4e392f9c1398225157e98d18f366f6687
vssense <70429263+vssense@users.noreply.github.com>
2020-08-31 16:18:09 +0500

    Update power 2 2.cpp

--- power 2 2.cpp
+++ power 2 2.cpp
@@ -2,11 +2,13 @@
 #include <math.h>
 #include <iso646.h>
 #include <assert.h>
-#define nosol -1
-#define linear -2
-#define infinity -3
-#define OneRoot 1
-#define TwoRoots 2
+
+enum {
+      NOSOL,
+      INFINITY_ROOTS,
+      ONE_ROOT,
+      TWO_ROOTS
+    };
 //-----------------------------------------------
 //! Solves a square equating ax2 + bx + c = 0
 //!  @param [in]   a     a-coefficient
@@ -17,69 +19,157 @@
 //!
 //!  @return Number of roots
 //!
-//!  @note infinity roots  -        infinity
+//!  @note infinity roots  -    infinity
 //!  @note linear equation -      linear
 //!  @note zero solutions  -       nosol
 //!  @note One root        -     OneRoot
 //!  @note Two roots       -    TwoRoots
 //-----------------------------------------------
-int Zero(double a)
+
+int IsZero(double a)
 {
     return (fabs(a) < 1e-6);
 }
-int korni(double a, double b, double c, double* x1, double* x2)
+
+
+int SolveLinear(double b, double c, double* x1)
 {
-assert (x1 != NULL);
-assert (x2 != NULL);
-assert (x1 != x2);
-    
-if ((Zero(a)) and (Zero(b)) and (Zero(b)))
-    return infinity;
-if (not(Zero(a)))
-    {
-    double d = 0;
-    d = b*b - 4*a*c;
-    if (d > 0)
+    assert (isfinite(b));
+    assert (isfinite(c));
+    assert (x1 != NULL);
+
+    if ((IsZero(b)) and (IsZero(c)))
+        return INFINITY_ROOTS;
+    if (b = 0)
+        return NOSOL;
+    else
         {
-        *x1 = (-b + sqrt(d)) / (2*a);
-        *x2 = (-b - sqrt(d)) / (2*a);
-        return TwoRoots;
+        *x1 = (-1*c/b);
+        return ONE_ROOT;
         }
-    else if (d == 0)
-        {
-        *x1 =  (-1*b)/(2*a);
-        *x2 =  (-1*b)/(2*a);
-        return OneRoot;
-        }
-    else return nosol;
-    }
-else
-    {
-    *x1 = -c/b;
-    return linear;
-    }
 }
+
+
+int SolveSquare(double a, double b, double c, double* x1, double* x2)
+{
+    assert (isfinite(a));
+    assert (isfinite(b));
+    assert (isfinite(c));
+
+    assert (x1 != NULL);
+    assert (x2 != NULL);
+    assert (x1 != x2);
+
+    if (IsZero(a))
+          return SolveLinear(b, c, x1);
+    else
+        {
+        double d = b*b - 4*a*c;
+        if (IsZero(d))
+            {
+            *x1 =  (-1*b)/(2*a);
+            *x2 =  (-1*b)/(2*a);
+            return ONE_ROOT;
+            }
+        else if (d > 0)
+            {
+            *x1 = (-b + sqrt(d)) / (2*a);
+            *x2 = (-b - sqrt(d)) / (2*a);
+            return TWO_ROOTS;
+            }
+        else return NOSOL;
+        }
+}
+
+
+void Test_SolveSquare();
+
+
 int main()
 {
-        printf("This is program to solve power 2 equations\n");
-        printf("Put coefficients with space\n");
+//  Test_SolveSquare();
+//  return 0;
+    printf("This is program to solve power 2 equations\n");
+    printf("Put coefficients with space\n");
 
-    double a, b, c, x1, x2;
-        a = b = c = x1 = x2 = 0;
-        scanf("%lg %lg %lg", &a, &b, &c);
+    double a  = 0,
+           b  = 0,
+           c  = 0,
+           x1 = 0,
+           x2 = 0;
 
-    int kolvo = korni(a, b, c, &x1, &x2);
-    if (kolvo == TwoRoots)
-        printf("x1 = %3.3lg\nx2 = %3.3lg", x1, x2);
-    else if (kolvo == OneRoot)
-        printf("x1 = x2 = %.3lg", x1);
-    else if (kolvo == nosol)
-        printf("This equation has no solutions");
-    else if (kolvo == linear)
-        printf("This is linear equation\n x = %.3lg",x1);
-    else if (kolvo == infinity)
-        printf("x belongs R");
-    else
-        printf("Something went wrong. Check korni");
-return 0;
+    scanf("%lg %lg %lg", &a, &b, &c);
+
+    int NumRoots = SolveSquare(a, b, c, &x1, &x2);
+
+    switch (NumRoots)
+        {
+        case NOSOL:
+            printf("This equation has no solutions");
+            break;
+        case ONE_ROOT:
+            printf("x = %.3lg", x1);
+            break;
+        case TWO_ROOTS:
+            printf("x1 = %3.3lg\nx2 = %3.3lg", x1, x2);
+            break;
+        case INFINITY_ROOTS:
+            printf("x belongs R");
+            break;
+        default:
+            printf("Something went wrong. Check SolveSquare");
+        }
+    return 0;
+}
+
+void Test_SolveSquare()
+{
+  double resx1  = 0,
+         resx2  = 0,
+         a      = 0,
+         b      = 0,
+         c      = 0,
+         realx1 = 0,
+         realx2 = 0;
+
+  resx1  =  2,
+  resx2  =  4,
+  a      =  1,
+  b      = -6,
+  c      =  8,
+          SolveSquare(a, b, c, &realx1, &realx2);
+          if (((resx1 == realx1) and (resx2 == realx2)) or
+              ((resx1 == realx2) and (resx2 == realx1)))
+                    printf("Test on line %d OK\n",__LINE__);
+          else printf(" Test on line %d failed\n"
+                      " expected x1 = %g x2 = %g\n"
+                      " real     x1 = %g x2 = %g\n",
+                      __LINE__, resx1, resx2, realx1, realx2);
+  resx1  =  3,
+  resx2  =  3,
+  a      =  1,
+  b      = -6,
+  c      =  9,
+          SolveSquare(a, b, c, &realx1, &realx2);
+          if (((resx1 == realx1) and (resx2 == realx2)) or
+              ((resx1 == realx2) and (resx2 == realx1)))
+                    printf("Test on line %d OK\n",__LINE__);
+          else printf(" Test on line %d failed\n"
+                      " expected x1 = %g x2 = %g\n"
+                      " real     x1 = %g x2 = %g\n",
+                      __LINE__, resx1, resx2, realx1, realx2);
+  resx1  =  2,
+  resx2  =  2,
+  a      =  0,
+  b      =  3,
+  c      = -6,
+          SolveSquare(a, b, c, &realx1, &realx2);
+          if (((resx1 == realx1) and (resx2 == realx2)) or
+              ((resx1 == realx2) and (resx2 == realx1)))
+                    printf("Test on line %d OK\n",__LINE__);
+          else printf("Test on line %d failed\n"
+                      " expected x1 = %g x2 = %g\n"
+                      " real     x1 = %g x2 = %g\n",
+                      __LINE__, resx1, resx2, realx1, realx2);
+
 }
