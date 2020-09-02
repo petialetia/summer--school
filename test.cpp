0cb726997edfa8d6091595680b9f0e776c7be714
vssense <70429263+vssense@users.noreply.github.com>
2020-09-02 16:21:32 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,9 +3,9 @@
 
 const int N = 100;
 
-void getline(char s[N], FILE* fp);
+void getline(char* s, FILE* fp, int NumS, int* flag);
 
-void putline(char s[N], FILE* fa);
+void putline(char* s, FILE* fa, int NumS, int* flag);
 
 int main()
 {
@@ -13,28 +13,43 @@ int main()
     FILE* fa;
     fp = fopen("test.txt","r");
     fa = fopen("output.txt","w");
+    char  s[N][N] = {0},{0};
+    int NumS = 0,
+        flag = 1;
 
-    char s[N] = {0};
-    getline(&s[N], fp);
-    putline(&s[N], fa);
+    while (flag)
+        {
+        getline(s, fp, NumS, &flag);
+        ++NumS;
+        }
+    flag = 1;
+    NumS = 0;
+
+    while (flag)
+        {
+        putline(s, fa, NumS, &flag);
+        ++NumS;
+        }
     return 0;
 }
 
-void getline(char s[N], FILE* fp)
+void getline(char* s, FILE* fp, int NumS, int* flag)
 {
 
     int  i = 0;
     char c = 0;
 
-    for (i = 0; (((s[i] = getc(fp)) != EOF) and (s[i] != '\n'));++i){}
-
-    s[i] = '\0';
+    for (i = 0; (((s[i][NumS] = getc(fp)) != EOF) and (s[i] != '\n'));++i)
+        if (s[i][NumS] = EOF)
+            *flag = 0;
+    if (s[i-1][NumS] != EOF)
+        s[i][NumS] = '\0';
 }
 
-void putline(char s[N], FILE* fa)
+void putline(char* s, FILE* fa, int NumS, int* flag)
 {
     int i = 0;
-    for (i = 0; (s[i] != '\0');++i)
+    for (i = 0; ((s[i] != '\0') and (s[i] != '\n'));++i)
         putc(s[i], fa);
 }
 
