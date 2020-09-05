8e4f157c0f761ea3a44da93fb7c00f0ce0a8738e
vssense <70429263+vssense@users.noreply.github.com>
2020-09-05 23:41:25 +0300

    Add files via upload

---
+++ dynamic_memory.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int len_of_file(FILE* in)
+{
+    fseek(in, 0, SEEK_END);
+    int pos = ftell(in);
+    fseek(in, 0, SEEK_SET);
+    return pos;
+}
+
+
+int* read_file(FILE* in, )
+{
+    int len = len_of_file(in);
+    int* start = calloc(len, sizeof(char))
+    fread(start, len * sizeof(char), len, in);
+    return start;
+}
+
+int main()
+{
+    FILE* in = fopen("input.txt","w");
+    read_file(in);
+
+
+    return 0;
+}
