9752148dd24dfbb35b5c1e3ce8b58d8621cfeaa8
vssense <70429263+vssense@users.noreply.github.com>
2020-09-05 23:49:22 +0300

    Update dynamic_memory.cpp

--- dynamic_memory.cpp
+++ dynamic_memory.cpp
@@ -10,18 +10,18 @@ int len_of_file(FILE* in)
 }
 
 
-int* read_file(FILE* in)
+char* read_file(FILE* in)
 {
     int len = len_of_file(in);
-    int* start = calloc(len, sizeof(char))
-    fread(start, len * sizeof(char), len, in);
+    char* start = calloc(len, sizeof(char))
+    fread(start, sizeof(char), len, in);
     return start;
 }
 
 int main()
 {
     FILE* in = fopen("input.txt","w");
-    int* start = read_file(in);
+    char* start = read_file(in);
 
 
     return 0;
