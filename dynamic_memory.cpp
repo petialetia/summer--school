46dabc52f518b08b8fefd5fb2d94b8997a88725d
vssense <70429263+vssense@users.noreply.github.com>
2020-09-06 11:22:32 +0300

    Update dynamic_memory.cpp

--- dynamic_memory.cpp
+++ dynamic_memory.cpp
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+const int N = 100;
+
 int len_of_file(FILE* in)
 {
     fseek(in, 0, SEEK_END);
@@ -13,16 +15,29 @@ int len_of_file(FILE* in)
 char* read_file(FILE* in)
 {
     int len = len_of_file(in);
-    char* start = calloc(len, sizeof(char))
+    char* start = (char*)calloc(len, sizeof(char));
     fread(start, sizeof(char), len, in);
     return start;
 }
 
+
+void read_in_2D(char* start, char* A[][N])
+{
+A[0][0] = start[0];
+
+}
+
+
 int main()
 {
-    FILE* in = fopen("input.txt","w");
+    FILE* in = fopen("input.txt","r");
     char* start = read_file(in);
+    char A[N][N] = {0};
+    read_in_2D(start, A[][N]);
 
+    printf("%s", A);
+
+    free(start);
 
     return 0;
 }
