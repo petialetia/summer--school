a94db13eef28f4a955ab1f78ead5df2866088449
vssense <70429263+vssense@users.noreply.github.com>
2020-09-18 22:44:20 +0300

    Update dynamic_memory.cpp

--- dynamic_memory.cpp
+++ dynamic_memory.cpp
@@ -28,7 +28,7 @@ char* read_file(FILE* in)
 {
     assert(in != NULL);
     uint64_t len = len_of_file(in);
-    char* start = (char*)calloc(len + 1, sizeof(char));
+    char* start  = (char*)calloc(len + 1, sizeof(char));
     fread(start, sizeof(char), len, in);
     return start;
 }
@@ -38,24 +38,29 @@ void make_lines(char* start, char** lines)
 {
     assert(start != NULL);
     assert(lines != NULL);
-    
-    int j = 1;
+
+    int lastI = 0;
+    int j     = 0;
+
+    lines[j++] = start;
     for (int i = 0; start[i] != EOF; ++i)
     {
         if (start[i] == '\n')
         {
             start[i] = '\0';
+            //LenLine[j] = i - lastI;
+            lastI = i;
             lines[j++] = &start[i+1];
         }
     }
 }
 
 
-void print_lines(const char* const* const lines, int num, FILE* out)
+void print_lines(const char* const* const lines, int num, FILE* const out)
 {
     assert(lines != NULL);
     assert(out   != NULL);
-    
+
     for (int j = 0; j < num; j++)
         fprintf(out, "%s\n", lines[j]);
 }
@@ -65,38 +70,31 @@ int strcmp_void(const void* line1, const void* line2)
 {
     assert(line1 != NULL);
     assert(line2 != NULL);
-    
+
     return strcmp(*(const char**)line1, *(const char**)line2);
 }
 
 
-int SortFromEnd_cmp(const void* line1, const void* line2)
+int strcmp_FromEnd(const void* line1, const void* line2)
 {
     assert(line1 != NULL);
     assert(line2 != NULL);
-    
-    char* oldLine1 = *(char**)line1;
-    char* oldLine2 = *(char**)line2;
-    int k = strcmp(strrev(*(char**)line1), strrev(*(char**)line2));
-    line1 = oldLine1;
-    line2 = oldLine2;
-    return k;
+
 }
 
 
 int main()
 {
-    FILE* in  = fopen("input.txt", "r");
-    FILE* out = fopen("output.txt","w");
+    FILE* const in  = fopen("input.txt",  "r");
+    FILE* const out = fopen("output.txt", "w");
     char* start = read_file(in);
 
     int num = num_lines(start);
-    char** lines = (char**)calloc(num + 1, sizeof(char**));
+    char** lines = (char**)calloc(num + 1, sizeof(char*));
 
-    lines[0] = start;
     make_lines(start, lines);
 
-    qsort(lines, num, sizeof(char*), SortFromEnd_cmp);
+    qsort(lines, num, sizeof(char*), strcmp_void);
 
     print_lines(lines, num, out);
 
