4209e63db0a92fe987c2d1b0f5c523bca6abfe9c
vssense <70429263+vssense@users.noreply.github.com>
2020-09-19 18:28:05 +0300

    Update dynamic_memory.cpp

--- dynamic_memory.cpp
+++ dynamic_memory.cpp
@@ -2,7 +2,17 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+//#include "onegin.hpp"
 #include <assert.h>
+//#include "TxLib.h"
+
+
+struct str
+{
+    char* str_;
+    int   len_ =   0 ;
+};
+
 
 uint64_t len_of_file(FILE* in)
 {
@@ -34,7 +44,7 @@ char* read_file(FILE* in)
 }
 
 
-void make_lines(char* start, char** lines)
+void make_lines(char* start, struct str** lines)
 {
     assert(start != NULL);
     assert(lines != NULL);
@@ -42,27 +52,28 @@ void make_lines(char* start, char** lines)
     int lastI = 0;
     int j     = 0;
 
-    lines[j++] = start;
+    (lines[j++])->str_ = start;
+    printf("%s", (lines[j++])->str_);
     for (int i = 0; start[i] != EOF; ++i)
     {
         if (start[i] == '\n')
         {
             start[i] = '\0';
-            //LenLine[j] = i - lastI;
+            (lines[j])->len_ = i - lastI;
             lastI = i;
-            lines[j++] = &start[i+1];
+            (lines[j++])->str_ = &start[i+1];
         }
     }
 }
 
 
-void print_lines(const char* const* const lines, int num, FILE* const out)
+void print_lines(struct str** lines, int num, FILE* const out)
 {
     assert(lines != NULL);
     assert(out   != NULL);
-
+    printf("sad");
     for (int j = 0; j < num; j++)
-        fprintf(out, "%s\n", lines[j]);
+        fprintf(out, "%s   %d\n", (lines[j])->str_, (lines[j])->len_);
 }
 
 
@@ -87,16 +98,17 @@ int main()
 {
     FILE* const in  = fopen("input.txt",  "r");
     FILE* const out = fopen("output.txt", "w");
+
     char* start = read_file(in);
 
     int num = num_lines(start);
-    char** lines = (char**)calloc(num + 1, sizeof(char*));
+    struct str* lines = (str*)calloc(num + 1, sizeof(str));
 
-    make_lines(start, lines);
+    make_lines(start, &lines);
 
-    qsort(lines, num, sizeof(char*), strcmp_void);
+    //qsort(lines, num, sizeof(char*), strcmp_void);
 
-    print_lines(lines, num, out);
+    print_lines(&lines, num, out);
 
     fclose(in );
     fclose(out);
