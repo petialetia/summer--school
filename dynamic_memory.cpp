a7cbf9532aefab86d02610a433e21ea6f11e5211
vssense <70429263+vssense@users.noreply.github.com>
2020-09-22 21:57:49 +0300

    Update dynamic_memory.cpp

--- dynamic_memory.cpp
+++ dynamic_memory.cpp
@@ -1,12 +1,13 @@
-#include <stdlib.h>
+#include <stdlib.h>            
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 #include <assert.h>
 #include <ctype.h>
+//#include <TxLib.h>
 
-#define ok   printf("Test in line %d OK\n",                                           __LINE__)
-#define fail printf("Test in line %d failed!!!!!!!!!!!!!!!!!!DDEEEBBAAAGGGG!!!!!!\n", __LINE__)
+#define OK   printf("Test in line %d OK\n",                                           __LINE__)
+#define FAIL printf("Test in line %d failed!!!!!!!!!!!!!!!!!!DDEEEBBAAAGGGG!!!!!!\n", __LINE__)
 
 struct str
 {
@@ -21,6 +22,7 @@ struct str
 //!
 //!  @return lenght of file
 //-----------------------------------------------
+
 uint64_t len_of_file(FILE* in)
 {
     assert(in != nullptr);
@@ -58,14 +60,14 @@ int num_lines(char* start, uint64_t num_symbols)
 {
     assert(start != nullptr);
 
-    int j = 1;
+    int num = 1;
     for (uint64_t i = 0; i < num_symbols; ++i)
-        if (start[i] == '\n') j++;
-    return j;
+        if (start[i] == '\n') num++;
+    return num;
 }
 
 //-----------------------------------------------
-//!  calculating number of lines
+//!  reading from file to buffer
 //!
 //!  @param [in] in           file read from
 //!  @param [in] num_symbols  number of symbols in the text
@@ -113,7 +115,7 @@ void make_lines(char* start, str* lines, uint64_t num_symbols)
             lines[j++].str_ = &start[i+1];
         }
     }
-    lines[j - 1].len_ = num_symbols - lastI - 4;
+    lines[j - 1].len_ = num_symbols - lastI;
 }
 
 //-----------------------------------------------
@@ -152,19 +154,22 @@ int str_cmp_with_begin(const void* arg1, const void* arg2)
     assert(arg1 != nullptr);
     assert(arg2 != nullptr);
 
-    if ((*(str*)arg1).len_ == 0) return  1;
-    if ((*(str*)arg2).len_ == 0) return -1;
+    str argg1 = *(str*)arg1;
+    str argg2 = *(str*)arg2;
+
+    if (argg1.len_ == 0) return  1;
+    if (argg1.len_ == 0) return -1;
 
 
-    while ((!(isletter((*((str*)arg1)).str_[i]))) && (i < (*(str*)arg1).len_)) i++;
-    while ((!(isletter((*((str*)arg2)).str_[j]))) && (j < (*(str*)arg2).len_)) j++;
+    while ((!(isletter(argg1.str_[i]))) && (i < argg1.len_)) i++;
+    while ((!(isletter(argg2.str_[j]))) && (j < argg2.len_)) j++;
 
-    while (tolower((*(str*)arg1).str_[i]) == tolower((*(str*)arg2).str_[j]))
+    while (tolower(argg1.str_[i]) == tolower(argg2.str_[j]))
     {
         i++;
         j++;
     }
-    return (tolower((*(str*)arg1).str_[i]) - tolower((*(str*)arg2).str_[j]));
+    return (tolower(argg1.str_[i]) - tolower(argg2.str_[j]));
 }
 
 //-----------------------------------------------
@@ -182,23 +187,49 @@ int str_cmp_with_end(const void* arg1, const void* arg2)
     int i = (*(str*)arg1).len_,
         j = (*(str*)arg2).len_;
 
+    str argg1 = *(str*)arg1;
+    str argg2 = *(str*)arg2;
+
     assert(arg1 != nullptr);
     assert(arg2 != nullptr);
 
     if (i == 0) return  1;
     if (j == 0) return -1;
 
-    while ((!(isletter((*(str*)arg1).str_[i]))) && (i > 0)) i--;
-    while ((!(isletter((*(str*)arg2).str_[j]))) && (j > 0)) j--;
+    while ((!(isletter(argg1.str_[i]))) && (i > 0)) i--;
+    while ((!(isletter(argg2.str_[j]))) && (j > 0)) j--;
 
-    while (tolower((*(str*)arg1).str_[i]) == tolower((*(str*)arg2).str_[j]))
+    while (tolower(argg1.str_[i]) == tolower(argg2.str_[j]))
     {
         i--;
         j--;
     }
 
-    return tolower((*(str*)arg1).str_[i]) - tolower((*(str*)arg2).str_[j]);
+    return tolower(argg1.str_[i]) - tolower(argg2.str_[j]);
 }
+
+void sort_and_print(str* lines, int num_str, FILE* out, int num_symbols, char* start)
+{
+    qsort(lines, num_str, sizeof(str), str_cmp_with_begin);
+    print_lines(lines, num_str, out);
+
+    fprintf(out, "\n\n----------------------------------------------------------\n\n");
+
+    qsort(lines, num_str, sizeof(str), str_cmp_with_end);
+    print_lines(lines, num_str, out);
+
+    fprintf(out, "\n\n----------------------------------------------------------\n\n");
+
+    for (int i = 0; i < num_symbols; ++i)
+        if (start[i] == '\0')
+        {
+            putc('\n', out);
+            i++;
+        }
+        else putc(start[i], out);
+}
+
+
 
 void free_all(char** start, str** lines)
 {
@@ -233,13 +264,11 @@ int main()
     num_symbols = read_file(in, num_symbols, start);
     int num_str = num_lines(start, num_symbols);
 
-    struct str* lines = (str*)calloc(num_str + 1, sizeof(str));
+    str* lines = (str*)calloc(num_str + 1, sizeof(str));
 
     make_lines(start, lines, num_symbols);
 
-    qsort(lines, num_str, sizeof(str), str_cmp_with_begin);
-
-    print_lines(lines, num_str, out);
+    sort_and_print(lines, num_str, out, num_symbols, start);
 
     fclose(in );
     fclose(out);
@@ -251,34 +280,34 @@ int main()
 
 void Test_all()
 {
-Test_isletter();
+Test_isletter          ();
 Test_str_cmp_with_begin();
-Test_str_cmp_with_end();
-Test_len_of_file();
-Test_num_lines();
+Test_str_cmp_with_end  ();
+Test_len_of_file       ();
+Test_num_lines         ();
 }
 
 void Test_isletter()
 {
     printf("Test \"isletter\"\n");
 
-    if (isletter('a') == 1) ok;
-    else fail;
+    if (isletter('a') == 1) OK;
+    else FAIL;
 
-    if (isletter('г') == 1) ok;
-    else fail;
+    if (isletter('г') == 1) OK;
+    else FAIL;
 
-    if (isletter('G') == 1) ok;
-    else fail;
+    if (isletter('G') == 1) OK;
+    else FAIL;
 
-    if (isletter('Ы') == 1) ok;
-    else fail;
+    if (isletter('Ы') == 1) OK;
+    else FAIL;
 
-    if (isletter('?') == 0) ok;
-    else fail;
+    if (isletter('?') == 0) OK;
+    else FAIL;
 
-    if (isletter('6') == 0) ok;
-    else fail;
+    if (isletter('6') == 0) OK;
+    else FAIL;
 
 }
 
@@ -294,24 +323,24 @@ void Test_str_cmp_with_begin()
     test2.str_ = "abcdsfvcfew";
     test2.len_ =            11;
 
-    if (str_cmp_with_begin(&test1, &test2) < 0) ok;
-    else fail;
+    if (str_cmp_with_begin(&test1, &test2) < 0) OK;
+    else FAIL;
 
     test1.str_ = "().aabcdefg";
     test1.len_ =            11;
     test2.str_ = "abcdsfvcfew";
     test2.len_ =            11;
 
-    if (str_cmp_with_begin(&test1, &test2) < 0) ok;
-    else fail;
+    if (str_cmp_with_begin(&test1, &test2) < 0) OK;
+    else FAIL;
 
     test1.str_ =    "zzbcdefg";
     test1.len_ =             8;
     test2.str_ = "!_cdsfvcfew";
     test2.len_ =            11;
 
-    if (str_cmp_with_begin(&test1, &test2) > 0) ok;
-    else fail;
+    if (str_cmp_with_begin(&test1, &test2) > 0) OK;
+    else FAIL;
 }
 
 void Test_str_cmp_with_end()
@@ -326,24 +355,24 @@ void Test_str_cmp_with_end()
     test2.str_ = "abcdsfvcfew";
     test2.len_ =            11;
 
-    if (str_cmp_with_end(&test1, &test2) < 0) ok;
-    else fail;
+    if (str_cmp_with_end(&test1, &test2) < 0) OK;
+    else FAIL;
 
     test1.str_ = "().aabcdefg";
     test1.len_ =            11;
     test2.str_ = "absfvcfew..";
     test2.len_ =            11;
 
-    if (str_cmp_with_end(&test1, &test2) < 0) ok;
-    else fail;
+    if (str_cmp_with_end(&test1, &test2) < 0) OK;
+    else FAIL;
 
     test1.str_ =    "zzbcdefg";
     test1.len_ =             8;
     test2.str_ = "!_cdsfvcaa(";
     test2.len_ =            11;
 
-    if (str_cmp_with_end(&test1, &test2) > 0) ok;
-    else fail;
+    if (str_cmp_with_end(&test1, &test2) > 0) OK;
+    else FAIL;
 }
 
 void Test_len_of_file()
@@ -354,8 +383,8 @@ void Test_len_of_file()
     fclose(test);
     FILE* test_ = fopen("test.txt", "r");
 
-    if (len_of_file(test_) == 35) ok;
-    else fail;
+    if (len_of_file(test_) == 35) OK;
+    else FAIL;
 
 }
 
@@ -364,9 +393,10 @@ void Test_num_lines()
     printf("\nTest \"num_lines\"\n");
     char* str = "avada\n kedavra\n";
 
-    if (num_lines(str, 17) == 3) ok;
-    else fail;
+    if (num_lines(str, 17) == 3) OK;
+    else FAIL;
 }
+
 
 
 
