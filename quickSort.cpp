23b4c9ef9a3465f08871960663e72f32add23d6a
vssense <70429263+vssense@users.noreply.github.com>
2020-09-27 00:01:17 +0300

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -4,24 +4,23 @@
 
 const int N = 1000;
 
-int partition(int* begin, int size);
+int partition(int* begin, int size, int size_elem, int(*cmp(const void*, const void*)));
 
 void swap(void* pointer1, void* pointer2, int size)
 {
-    char* tmp = (char*)calloc(size, sizeof(char));
+    char tmp = '\0';
     for(int i = 0; i < size; ++i)
     {
-        tmp[i] = ((char*)pointer1)[i];
+        tmp = ((char*)pointer1)[i];
         ((char*)pointer1)[i] = ((char*)pointer2)[i];
-        ((char*)pointer2)[i] = tmp[i];
+        ((char*)pointer2)[i] = tmp;
     }
 }
 
 
-void quickSort(int* start, int size)
+void quickSort(int* start, int size, int size_elem, int(*cmp(const void*, const void*)))
 {
     assert(start != 0);
-    //assert(size != 0);
 
     if (size <= 1)
     {
@@ -29,7 +28,7 @@ void quickSort(int* start, int size)
     }
     if (size == 2)
     {
-        if (start[0] <= start[1])
+        if (cmp(start, start + 1) <= 0)
         {
             return;
         }
@@ -39,32 +38,32 @@ void quickSort(int* start, int size)
             return;
         }
     }
-    int pos = partition(start, size);
+    int pos = partition(start, size, size_elem, cmp);
 
-    quickSort(start, pos);
-    quickSort(start + pos + 1, size - pos - 1);
+    quickSort(start, pos, size_elem, cmp);
+    quickSort(start + pos + 1, size - pos - 1, size_elem, cmp);
 }
 
-int partition(int* begin, int size)
+int partition(int* begin, int size, int size_elem, int(*cmp)(const void*, const void*))
 {
     assert(begin != 0);
     int i = 1;
 
     for(int j = 1; j < size; ++j)
     {
-        if (begin[j] < begin[0])
+        if (cmp(begin, begin + j) > 0)     //>=
         {
-            swap(begin + j, begin + i, sizeof(int));
+            swap(begin + j, begin + i, size_elem);
             ++i;
         }
     }
-    swap(begin, begin + i - 1, sizeof(int));
+    swap(begin, begin + i - 1, size_elem);
     return i - 1;
 }
 
-int int_cmp(void* arg1, void* arg2)
+int int_cmp(const void* arg1, const void* arg2)
 {
-    return (int*)arg1 - (int*)arg2;
+    return (const int*)arg1 - (const int*)arg2;
 }
 
 
@@ -76,7 +75,7 @@ int main()
     {
         array[i] = rand()%213;
     }
-    quickSort(array, N);
+    quickSort(array, N, sizeof(int), int_cmp);
     for (int i = 0; i < N; ++i)
     {
         printf("%d  ", array[i]);
