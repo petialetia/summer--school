146f10fcda7186a9791d0ff1c4d29a6b9207bec1
vssense <70429263+vssense@users.noreply.github.com>
2020-09-27 00:55:15 +0300

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -4,7 +4,7 @@
 
 const int N = 1000;
 
-int partition(int* begin, int size, int size_elem, int(*cmp)(const void*, const void*));
+int partition(void* begin, int size, int size_elem, int(*cmp)(const void*, const void*));
 
 void swap(void* pointer1, void* pointer2, int size)
 {
@@ -18,9 +18,9 @@ void swap(void* pointer1, void* pointer2, int size)
 }
 
 
-void quickSort(int* start, int size, int size_elem, int(*cmp)(const void*, const void*))
+void quickSort(void* begin, int size, int size_elem, int(*cmp)(const void*, const void*))
 {
-    assert(start != 0);
+    assert(begin != 0);
 
     if (size <= 1)
     {
@@ -28,36 +28,36 @@ void quickSort(int* start, int size, int size_elem, int(*cmp)(const void*, const
     }
     if (size == 2)
     {
-        if (cmp(start, start + 1) <= 0)
+        if (cmp((char*)begin, (char*)begin + 1*size_elem) <= 0)
         {
             return;
         }
         else
         {
-            swap(start, start + 1, size_elem);
+            swap((char*)begin, (char*)begin + 1*size_elem, size_elem);
             return;
         }
     }
-    int pos = partition(start, size, size_elem, cmp);
+    int pos = partition(begin, size, size_elem, cmp);
 
-    quickSort(start, pos, size_elem, cmp);
-    quickSort(start + pos + 1, size - pos - 1, size_elem, cmp);
+    quickSort((char*)begin, pos, size_elem, cmp);
+    quickSort((char*)begin + (pos + 1)*size_elem, size - pos - 1, size_elem, cmp);
 }
 
-int partition(int* begin, int size, int size_elem, int(*cmp)(const void*, const void*))
+int partition(void* begin, int size, int size_elem, int(*cmp)(const void*, const void*))
 {
     assert(begin != 0);
     int i = 1;
 
     for(int j = 1; j < size; ++j)
     {
-        if (cmp(begin, begin + j) > 0)     //>=
+        if (cmp((char*)begin, (char*)begin + j*size_elem) > 0)     //>=
         {
-            swap(begin + j, begin + i, size_elem);
+            swap((char*)begin + j*size_elem, (char*)begin + i*size_elem, size_elem);
             ++i;
         }
     }
-    swap(begin, begin + i - 1, size_elem);
+    swap((char*)begin, (char*)begin + (i - 1)*size_elem, size_elem);
     return i - 1;
 }
 
