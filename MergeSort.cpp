85f776b9a91c1cf58fcee4bd7aa4611d62e9f7df
vssense <70429263+vssense@users.noreply.github.com>
2020-10-07 20:23:04 +0300

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,60 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
+---
 
-const int N = 10;
+Checks: '-*,readability-*'
 
-int* merge(int* arr1, int* arr2, int size1, int size2);
+WarningsAsErrors: '*'
 
-int* mergeSort(int* array, int size)
-{
-    if (size == 1) return array;
-    int* A = mergeSort(array, size/2);
-    int* B = mergeSort(array + size - size/2, size - size/2);
-
-    return merge(A, B, size/2, size - size/2);
-}
-
-int* merge(int* arr1, int* arr2, int size1, int size2)
-{
-    int i = 0;
-    int j = 0;
-    int* arr3 = (int*)calloc(size1 + size2, sizeof(int));
-
-    while(i < size1 && j < size2)
-    {
-        if (arr1[i] <= arr2[j])
-        {
-            arr3[i + j] = arr1[i++];
-        }
-        else
-        {
-            arr3[i + j] = arr2[j++];
-        }
-
-    }
-
-    while(i < size1)
-    {
-        arr3[i + j] = arr1[i++];
-    }
-    while(j < size2)
-    {
-        arr3[i + j] = arr2[j++];
-    }
-    return arr3;
-
-}
-
-
-int main()
-{
-    int array[N] = {1, 25, 5, 3, 9, 3, 1, 7, 5, 2};
-
-    int* newarray = mergeSort(array, N);
-    for (int i = 0; i < N; ++i)
-    {
-        printf("%d  ", newarray[i]);
-    }
-
-    return 0;
-}
+CheckOptions:
+  - key: readability-function-size.LineThreshold
+    value: '40'
+  - key: readability-function-size.ParameterThreshold
+    value: '5'
+  - key: readability-identifier-naming.ClassCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.ConstantCase
+    value: 'lower_case'
+  - key: readability-identifier-naming.EnumCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.EnumConstantCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.FunctionCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.MemberCase
+    value: 'lower_case'
+  - key: readability-identifier-naming.MethodCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.NamespaceCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.ParameterCase
+    value: 'lower_case'
+  - key: readability-identifier-naming.PrivateMemberSuffix
+    value: '_'
+  - key: readability-identifier-naming.ProtectedMemberSuffix
+    value: '_'
+  - key: readability-identifier-naming.PublicMemberSuffix
+    value: '_'
+  - key: readability-identifier-naming.StructCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.TypeAliasCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.TypedefCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.TypeTemplateParameterCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.UnionCase
+    value: 'CamelCase'
+  - key: readability-identifier-naming.ValueTemplateParameterCase
+    value: 'lower_case'
+  - key: readability-identifier-naming.VariableCase
+    value: 'lower_case'
+  - key: readability-magic-numbers.IgnoredIntegerValues
+    value: '1'
+  - key: readability-simplify-boolean-expr.ChainedConditionalReturn
+    value: '1'
+  - key: readability-simplify-boolean-expr.ChainedConditionalAssignment
+    value: '1'
